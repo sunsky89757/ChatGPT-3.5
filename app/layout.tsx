fdd1a54d87ea930aa7918fc045b3f6bad059f8f3
sunsky89757 <113254524+sunsky89757@users.noreply.github.com>
2023-12-15 22:12:03 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "ChatGPT-3.5 蛋壳部署",
+  description: "永久免费可用的ChatGTP-3.5程序",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "ChatGPT-3.5 蛋壳部署",
     statusBarStyle: "default",
   },
 };
